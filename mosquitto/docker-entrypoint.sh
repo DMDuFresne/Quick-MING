23551ada3ba55a97d89110be421ddeb58997964e
DMDuFresne <Dylan@DuFresne.Social>
2024-03-09 12:28:46 -0600

    Added Telegraf

--- mosquitto/docker-entrypoint.sh
+++ mosquitto/docker-entrypoint.sh
@@ -2,7 +2,8 @@
 
 # Create Mosquitto username and password
 if [ -n "${MOSQUITTO_USERNAME}" ] && [ -n "${MOSQUITTO_PASSWORD}" ]; then
-    mosquitto_passwd -b /mosquitto/config/mosquitto.passwd "${MOSQUITTO_USERNAME}" "${MOSQUITTO_PASSWORD}"
+    mosquitto_passwd -b -c /mosquitto/config/pass "${MOSQUITTO_USERNAME}" "${MOSQUITTO_PASSWORD}"
+    chown mosquitto:mosquitto /mosquitto/config/pass
 fi
 
 # Execute the main CMD from the Dockerfile
